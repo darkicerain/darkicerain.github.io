c94016c6396fdcb8e36abffd6a9286ffbd23bbd9
darkicerain <343443047@qq.com>
2015-10-20 10:26:15 +0800

    添加多说，测试

--- _plugins/admin.rb
+++ _plugins/admin.rb
@@ -1,13 +1,16 @@
 require 'rubygems'
-
-
 require 'webrick'
+require 'json'
+require_relative '../run'
 
 server = WEBrick::HTTPServer.new :Port => 8000
-server.mount_proc '/' do |req, res|
-  res.body = 'Hello, world!'
-end
-
+#server.mount_proc '/admin' do |req, res|
+#  run=Run.new()
+#  puts req["test"]
+#  result= run.add_post({})
+#  res.body=result.to_json()
+#end
+server.mount('/', WEBrick::HTTPServlet::CGIHandler, '/')
 trap('INT') { server.stop } # stop server with Ctrl-C
 server.start
 
